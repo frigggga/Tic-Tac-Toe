1fb438bfbf7c14782e2d48a3f90ccb768730be6c
Isha Zhang <ishz@uchicago.edu>
2024-01-31 03:43:02 -0600

    finished project4

---
+++ Tic-Tac-Toe/Grid.swift
@@ -0,0 +1,8 @@
+//
+//  Grid.swift
+//  Tic-Tac-Toe
+//
+//  Created by Zhang on 2024/1/30.
+//
+
+import Foundation
---
+++ Tic-Tac-Toe/InfoView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoView.swift
+//  Tic-Tac-Toe
+//
+//  Created by Zhang on 2024/1/30.
+//
+
+import UIKit
+
+class InfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
